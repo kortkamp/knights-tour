e92155a3e7bc10e18faaf7241552e7c8a804dc56
kortkamp <marcelusmedius@gmail.com>
2022-10-21 14:33:02 -0300

    feat: place controls aside

--- scripts/App.js
+++ scripts/App.js
@@ -6,6 +6,15 @@ import { placeMark, removeMark } from "./graphics.js";
 
 const board = new Board([]);
 
+let delay = 500;
+
+function setSpeed(speed){
+  console.log(speed)
+  console.log(1000 - speed*10)
+  
+  delay = 1000 - speed*10
+}
+
 let pos = [0,0]
 
 board.placePiece(pos, Pieces.knight)
@@ -14,6 +23,8 @@ board.placePiece(pos, Pieces.knight)
 
 
 export function tryMove(){
+
+  const deadEnds = []
   
   const validMoves = board.getValidMoves(pos);
 
@@ -28,11 +39,23 @@ export function tryMove(){
   placeMark(pos)
   board.movePiece(pos, newMove);
   pos = newMove;
+
+  console.log(delay)
+
+  if(delay){
+    setTimeout(tryMove, delay);
+  }
+
 }
 
 let btn = document.querySelector('#test');
 btn.addEventListener('click', function () {
   tryMove();
+});
+
+let speedRange = document.querySelector('#speed');
+speedRange.addEventListener('mouseup', function (e) {
+  setSpeed(e.target.value)
 });
 
 console.log('Game Loaded')
